3eaa672e751d5cb6d97661617cdceccb3bda203b
code-withRahul <rahul379@amityonline.com>
2022-11-08 19:25:38 +0530

    added some cpp program

---
+++ aaabsd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+	using namespace std;
+	int i ;
+	cout<<"Enter any digit\n";
+	cin>>i;
+for ( int  i=0;  i<50;  i++)
+
+	getch();
+	return 0;
+}
---
+++ mm.cpp
@@ -0,0 +1,19 @@
+// C++ program to sizes of data types
+#include<iostream>
+using namespace std;
+
+int main(){
+float a,b,c,d,e;
+cout<<"enter the first digit/n"<<"    Enter the second digit"<<"    Enter the third digit"<<"    Enter the fourth digit"<<"    Enter the fifth digit";
+cin>>a>>b>>c>>d>>e;
+cout<<(a+b+c+d+e)/5;/* result declaration 
+	Name: 
+	Copyright: 
+	Author: 
+	Date: 30/06/21 12:45
+	Description: 
+*/
+
+return 0;
+}
+
